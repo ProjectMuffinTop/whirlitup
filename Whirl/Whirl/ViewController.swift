bba654494da636478e7440bd2ed712b4609324a6
Andrew Escobedo <faescobedo28@gmail.com>
2016-06-25 23:07:30 -0700

    Initial Commit. XCode creation.

---
+++ Whirl/Whirl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whirl
+//
+//  Created by Andrew Escobedo on 6/25/16.
+//  Copyright © 2016 Project Muffin Top. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
